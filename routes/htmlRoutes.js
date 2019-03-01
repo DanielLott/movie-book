3fd5cfd9916a6dba723ee8995526c4f1edfd98b4
DBarton <42483562+DylanBarton1856@users.noreply.github.com>
2019-02-28 20:45:57 -0700

    workingHTMLRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,28 @@
 var db = require("../models");
+// var path = require("path");
+// // Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/user", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("userHome", {
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/movies", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("movies", {
+        examples: dbExamples
       });
     });
   });
