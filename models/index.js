86601c9e4aff34ce43e834021bd9c8d6133ad387
Dan Lott <danieljlott@live.com>
2019-03-06 21:41:01 -0700

    adjusting connection

--- models/index.js
+++ models/index.js
@@ -4,26 +4,29 @@ var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || 'production';
-var config = require(__dirname + '/../config/config.json')[env];
-const host = process.env.SERVER || config.host
-const database = process.env.NAME || config.database
-const username = process.env.USERNAME || config.username
-const password = process.env.PASSWORD || config.password
-const dialect = process.env.DIALECT || config.dialect
+// var env = process.env.NODE_ENV || 'production';
+// var config = require(__dirname + '/../config/config.json')[env];
+// const host = process.env.SERVER || config.host
+// const database = process.env.NAME || config.database
+// const username = process.env.USERNAME || config.username
+// const password = process.env.PASSWORD || config.password
+// const dialect = process.env.DIALECT || config.dialect
 
 var db = {};
 
-if (config.use_env_variable) {
-    var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-    var sequelize = new Sequelize(database, config.username, password, {
-        host,
-        dialect,
-        logging: false,
-        operatorsAliases: false
-    });
-}
+var sequelize = new Sequelize(processs.env.JAWSDB_URL);
+
+
+// if (config.use_env_variable) {
+//     var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//     var sequelize = new Sequelize(database, config.username, password, {
+//         host,
+//         dialect,
+//         logging: false,
+//         operatorsAliases: false
+//     });
+// }
 
 fs
     .readdirSync(__dirname)
